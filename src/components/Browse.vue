7b70386c0b43f875d93dc93e585ff6f4baf6a970
marvinvihman <marvin@ut.ee>
2020-11-23 01:38:18 +0200

    browse profiles code

--- src/components/Browse.vue
+++ src/components/Browse.vue
@@ -3,13 +3,35 @@
   <header>
   </header>
   <section class="main-container">
-
+    <div v-for="profile in profiles" :key="profile.id">
+      <div class="profile">
+        <img :src="profile.avatar" :alt="profile.firstname + ' ' + profile.lastname">
+        <h2>{{ profile.firstname + " " + profile.lastname }}</h2>
+        <button class="follow-button">Follow</button>
+      </div>
+    </div>
   </section>
   </body>
 </template>
 
 <script>
+import store from "@/store";
+
 export default {
-name: "Browse"
+  store,
+  computed: {
+    posts(){
+      return this.$store.getters["profiles/profiles"]
+    }
+  },
+  mounted() {
+    this.$store.dispatch("profiles/getProfiles")
+  },
+  methods: {
+    liked: function(button){
+      button.classList.toggle("followed")
+    }
+  },
+  name: "Browse"
 }
 </script>
